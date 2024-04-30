c28d71047304f8ff1241bb820c6062846324c61d
Bernhart46 <hegyiferenc@proton.me>
2024-04-30 10:32:44 +0200

    Remove console.log
    Plus added a comment to the calcNewHeight function.

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,7 +1,6 @@
+//This is for calculating the Height of the inputs (example: pressing enter, creating new line)
 export const calcNewHeight = (value: string) => {
   const numberOfLines = value.split("\n").length;
-
-  console.log("rerendered");
 
   return numberOfLines * 16 * 1.5;
 };
