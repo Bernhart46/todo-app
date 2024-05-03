8b137b6608d887624aefb9851f849b2fbdde3c39
Bernhart46 <hegyiferenc@proton.me>
2024-05-03 13:35:15 +0200

    Fix Script issue
    The old groups didn't contain the "sort" attribute. Now it creates a "default" sort attribute for it IF the sort attr does not exist already.

--- src/scripts/renew-old-saves.ts
+++ src/scripts/renew-old-saves.ts
@@ -1,4 +1,4 @@
-import { todoChild, todoGroup } from "../store/todo/todo-slice";
+import { sorts, todoChild, todoGroup } from "../store/todo/todo-slice";
 
 export function renewOldSaves(oldData: todoGroup[]): todoGroup[] {
   const newData = oldData.map((todoGroup) => {
@@ -10,6 +10,11 @@ export function renewOldSaves(oldData: todoGroup[]): todoGroup[] {
           status: todoChild.status.toLowerCase(),
         } as todoChild;
       }),
+      sort: todoGroup.sort || {
+        not_started: "random" as sorts,
+        in_progress: "random" as sorts,
+        done: "random" as sorts,
+      },
     };
   });
 
